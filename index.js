b765d65a9b75c2797728dca2401fba6632598280
Luis Escalona <luiscescalona@gmail.com>
2021-08-30 15:30:15 -0300

    Funcion sum

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 function showNumber(number) {
     console.log("El número ingresado es " + number);
 };
+
+function sum(num1, num2) {
+    let total = num1 + num2;
+    console.log("La sumatoria es " + total);
+};
